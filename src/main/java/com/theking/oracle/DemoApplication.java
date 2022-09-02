3cff1c436b0456359945152426cfb181e75c5112
chenjinsheng <king524821@163.com>
2022-09-02 17:30:50 +0800

    feat:add maven dependency

--- src/main/java/com/theking/oracle/DemoApplication.java
+++ src/main/java/com/theking/oracle/DemoApplication.java
@@ -3,6 +3,9 @@ package com.theking.oracle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author theking
+ */
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
